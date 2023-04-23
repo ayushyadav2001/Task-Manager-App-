2547bc2c85145ed2692f7c4c32e34c6434a53cd2
Ayush Yadav <infoayushyadav96@gmail.com>
2023-04-23 12:21:29 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import AddTask from "./components/AddTask";
+import TaskItem from "./components/TaskItem";
 
 export default function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [taskList, setTaskList] = useState([]);
+
+  function showModalHandler() {
+    setShowModal(true);
+  }
+  function hideModalHandler() {
+    setShowModal(false);
+  }
+  function addNewTask(newTask) {
+    setTaskList((currentTaskList) => [
+      ...currentTaskList,
+      { text: newTask, id: Math.random().toString() },
+    ]);
+    hideModalHandler();
+  }
+
+  function deleteTask(id) {
+    setTaskList((currentTaskList) => {
+      return currentTaskList.filter((task) => task.id !== id); //false condn item gets removed
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <Button
+        title="Add New Task"
+        color={"#f5ce42"}
+        onPress={showModalHandler}
+      />
+      <AddTask
+        addNewTask={addNewTask}
+        visible={showModal}
+        hideModal={hideModalHandler}
+      />
+      <View style={styles.taskListSection}>
+        {taskList.length > 0 ? (
+          <Text style={styles.taskOverviewTitle}>Your Tasks!</Text>
+        ) : (
+          <Text></Text>
+        )}
+        <FlatList
+          data={taskList}
+          renderItem={({ item, index }) => {
+            return (
+              <TaskItem item={item} index={index} onDeleteTask={deleteTask} />
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
+        {/*<ScrollView>
+          {
+            taskList.map((taskItem, index) => {
+              return (
+                <View style={styles.taskItemStyle} key={index}>
+                  <Text style={styles.taskItemTextStyle}>{index + 1}: {taskItem}</Text>
+                </View>
+              )
+            })
+          }
+        </ScrollView>*/}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 45,
+    paddingHorizontal: 15,
+    backgroundColor: "skyblue",
+  },
+  taskListSection: {
+    flex: 6,
+  },
+  taskOverviewTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
   },
 });
